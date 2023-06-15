fbfa722256a2a985043edef44e09b2110396ae6e
Quentin Goujon <quentin.goujon@epita.fr>
2023-06-15 16:28:42 +0200

    exemple

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,42 +1,42 @@
 <style>
 	#app {
-	  display: grid;
-	  grid-template-rows: 1fr 6fr 1fr;
-	  grid-template-columns: 1fr 4fr 1fr;
+
 	  height: 100vh;
 	  width: 100vw;
 	}
-  
+
 	#header {
 	  grid-column: 1 / span 3;
 	  background-color: #ddd;
 	}
-  
+
 	#left {
 	  grid-row: 2 / span 2;
 	  background-color: #eee;
 	}
-  
+
 	#middle {
 	  background-color: #f8f8f8;
+		flex-grow: 3;
 	}
-  
+
 	#right {
 	  background-color: #ddd;
+		flex-grow: 1;
 	}
-  
+
 	#footer {
 	  grid-column: 2 / span 2;
 	  background-color: #eee;
 	}
   </style>
-  
+
   <script lang="ts">
 	  import PropertiesBar from '../annimations/components/PropertiesBar.svelte';
 	  import { WAVE_MODULES } from '../annimations/index.js';
-  
+
 	  let modules = WAVE_MODULES;
-  
+
 	  let createConfig = (modules) => {
 		  let config = {};
 		  modules.forEach((module) => {
@@ -45,19 +45,22 @@
 		  });
 		  return config;
 	  };
-  
+
 	  let logConfig = () => {
 		  console.log(createConfig(modules));
 	  };
   </script>
-  
+
   <div id="app">
 	<div id="header">Entête</div>
 	<div id="left">Gauche</div>
-	<div id="middle">Milieu</div>
-	<div id="right">
+	  <div class="flex-row">
+
+	<div id="middle" >Milieu</div>
+	<div id="right" >
 	  <PropertiesBar bind:modules />
 	</div>
+
+	  </div>
 	<div id="footer">Pied de page</div>
   </div>
-  
