47034b3d0a727e6a9d913e1938de1013d76bacb6
Quentin Goujon <quentin.goujon@epita.fr>
2023-06-17 12:51:12 +0200

    supbase

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,71 +1,85 @@
+<script lang="ts">
+	import PropertiesBar from '../annimations/components/PropertiesBar.svelte';
+	import { WAVE_MODULES } from '../annimations/index.js';
+	import ListBlockSlector from '$lib/components/ListBlockSlector.svelte';
+	// import type { Session } from '@supabase/supabase-js';
+	// import { onMount } from 'svelte';
+	// import { supabase } from '../supabaseClient';
+	// import Auth from '$lib/components/Auth.svelte';
+	// import Account from '$lib/components/Account.svelte';
+
+	let modules = WAVE_MODULES;
+
+	let createConfig = (modules) => {
+		let config = {};
+		modules.forEach((module) => {
+			if (module.range) config[module.name] = module.range.value;
+			else if (module.color) config[module.name] = module.color.value;
+		});
+		return config;
+	};
+
+	// let session: Session | null;
+	// onMount(() => {
+	// 	supabase.auth.getSession().then(({ data }) => {
+	// 		session = data.session;
+	// 	});
+	//
+	// 	supabase.auth.onAuthStateChange((_event, _session) => {
+	// 		session = _session;
+	// 	});
+	// });
+</script>
+
+<div id="app">
+	<!--	  <div class="container" style="padding: 50px 0 100px 0">-->
+	<!--		  {#if !session}-->
+	<!--			  <Auth />-->
+	<!--		  {:else}-->
+	<!--			  <Account {session} />-->
+	<!--		  {/if}-->
+	<!--	  </div>-->
+
+	<div class="flex-row">
+		<div id="left">
+			<ListBlockSlector />
+		</div>
+
+		<div id="middle">Milieu</div>
+		<div id="right">
+			<PropertiesBar bind:modules />
+		</div>
+	</div>
+	<div id="footer">Pied de page</div>
+</div>
+
 <style>
 	#app {
-
-	  height: 100vh;
-	  width: 100vw;
+		height: 100vh;
+		width: 100vw;
 	}
 
 	#header {
-
-	  background-color: #ddd;
+		background-color: #ddd;
 	}
 
 	#left {
 		width: min-content;
-	  background-color: #eee;
+		background-color: #eee;
 	}
 
 	#middle {
-	  background-color: #f8f8f8;
+		background-color: #f8f8f8;
 		flex-grow: 3;
 	}
 
 	#right {
-	  background-color: #ddd;
+		background-color: #ddd;
 		flex-grow: 1;
 	}
 
 	#footer {
-	  grid-column: 2 / span 2;
-	  background-color: #eee;
+		grid-column: 2 / span 2;
+		background-color: #eee;
 	}
-  </style>
-
-  <script lang="ts">
-	  import PropertiesBar from '../annimations/components/PropertiesBar.svelte';
-	  import { WAVE_MODULES } from '../annimations/index.js';
-	  import ListBlockSlector from "$lib/components/ListBlockSlector.svelte";
-
-	  let modules = WAVE_MODULES;
-
-	  let createConfig = (modules) => {
-		  let config = {};
-		  modules.forEach((module) => {
-			  if (module.range) config[module.name] = module.range.value;
-			  else if (module.color) config[module.name] = module.color.value;
-		  });
-		  return config;
-	  };
-
-	  let logConfig = () => {
-		  console.log(createConfig(modules));
-	  };
-  </script>
-
-  <div id="app">
-
-	  <div class="flex-row">
-	<div id="left">
-
-		<ListBlockSlector/>
-	</div>
-
-
-	<div id="middle" >Milieu</div>
-	<div id="right" >
-	  <PropertiesBar bind:modules />
-	</div>
-
-	  </div>
-	<div id="footer">Pied de page</div>
-  </div>
+</style>
