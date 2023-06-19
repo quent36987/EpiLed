2c56ee3caea0f2d10dea7dc8b1e3c3f6d07ca087
Angmar <42914702+Aarcheon@users.noreply.github.com42914702+Aarcheon@users.noreply.github.com>
2023-06-19 19:03:23 +0200

    added button component and fixed some things

--- src/routes/devices/+page.svelte
+++ src/routes/devices/+page.svelte
@@ -1,57 +1,89 @@
 <script lang="ts">
-	import ListBlockSlector from '$lib/components/ListBlockSlector.svelte';
-	import PropertiesBar from '../../annimations/components/PropertiesBar.svelte';
-	import { WAVE_MODULES } from '../../annimations';
+    import ListBlockSlector from '$lib/components/ListBlockSlector.svelte';
+    import PropertiesBar from '../../annimations/components/PropertiesBar.svelte';
+    import Button from '../../lib/components/common/Button.svelte';
+    import { WAVE_MODULES } from '../../annimations';
 
-	let modules = WAVE_MODULES;
+    let modules = WAVE_MODULES;
+    let switchState = false;
 
-	let createConfig = (modules) => {
-		let config = {};
-		modules.forEach((module) => {
-			if (module.range) config[module.name] = module.range.value;
-			else if (module.color) config[module.name] = module.color.value;
-		});
-		return config;
-	};
+    let createConfig = (modules) => {
+        let config = {};
+        modules.forEach((module) => {
+            if (module.range) config[module.name] = module.range.value;
+            else if (module.color) config[module.name] = module.color.value;
+        });
+        return config;
+    };
+
+    const handleSwitch = () => {
+        switchState = !switchState;
+    }
+
+    const handleLoad = () => {
+        console.log("Load button clicked");
+    }
+
+    const handleDelete = () => {
+        console.log("Delete button clicked");
+    }
+
+    const handleSave = () => {
+        console.log("Save button clicked");
+    }
 </script>
 
 <div id="app">
-	<div class="flex-row">
-		<div id="left">
-			<ListBlockSlector />
-		</div>
+    <div class="flex-row">
+        <div id="left">
+            <ListBlockSlector />
+        </div>
 
-		<div id="middle">Milieu</div>
-		<div id="right">
-			<PropertiesBar bind:modules />
-		</div>
-	</div>
-	<div id="footer">Pied de page</div>
+        <div id="middle">Milieu</div>
+        <div id="right">
+            <PropertiesBar bind:modules />
+        </div>
+    </div>
+    <div id="footer">
+        <Button text={switchState ? "Off" : "On"} handleClick={handleSwitch} />
+        <Button text="Load" handleClick={handleLoad} />
+        <Button text="Delete" handleClick={handleDelete} />
+        <Button text="Save" handleClick={handleSave} />
+    </div>
 </div>
 
+
 <style>
-	#app {
-		height: 100vh;
-		width: 100vw;
-	}
+    #app {
+        height: 100vh;
+        width: 100vw;
+    }
 
-	#left {
-		width: min-content;
-		background-color: #eee;
-	}
+    #left {
+        width: min-content;
+        background-color: #eee;
+    }
 
-	#middle {
-		background-color: #f8f8f8;
-		flex-grow: 3;
-	}
+    #middle {
+        background-color: #f8f8f8;
+        flex-grow: 3;
+    }
 
-	#right {
-		background-color: #ddd;
-		flex-grow: 1;
-	}
+    #right {
+        background-color: #ddd;
+        flex-grow: 1;
+    }
 
-	#footer {
-		grid-column: 2 / span 2;
-		background-color: #eee;
-	}
+    #footer {
+        grid-column: 2 / span 2;
+        background-color: #eee;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        padding: 100px 5px;
+    }
+
+    #footer Button {
+        background: none !important;
+    }
 </style>
